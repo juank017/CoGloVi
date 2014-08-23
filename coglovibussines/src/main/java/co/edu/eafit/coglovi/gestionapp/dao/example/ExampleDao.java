8c3c54a39be216d1931724ca423493f6d58f6ea9
juank017 <juank017@hotmail.com>
2014-08-23 12:37:45 -0500

    Se elimina archivos no necesarios

--- coglovibussines/src/main/java/co/edu/eafit/coglovi/gestionapp/dao/example/ExampleDao.java
+++
@@ -1,7 +0,0 @@
-package co.edu.eafit.coglovi.gestionapp.dao.example;
-
-public interface ExampleDao {
-
-	void selectSysdate();
-
-}
--- coglovibussines/src/main/java/co/edu/eafit/coglovi/gestionapp/manager/example/ExampleManager.java
+++
@@ -1,9 +0,0 @@
-package co.edu.eafit.coglovi.gestionapp.manager.example;
-
-public interface ExampleManager {
-
-	void selecExample();
-
-	void save();
-
-}
