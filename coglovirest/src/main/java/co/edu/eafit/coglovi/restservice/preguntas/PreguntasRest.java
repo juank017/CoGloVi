979ccdb62fb32e413621ff3a71664079cea29855
juank017 <juank017@hotmail.com>
2014-09-16 17:26:39 -0500

    Update PreguntasRest.java

--- coglovirest/src/main/java/co/edu/eafit/coglovi/restservice/preguntas/PreguntasRest.java
+++ coglovirest/src/main/java/co/edu/eafit/coglovi/restservice/preguntas/PreguntasRest.java
@@ -3,6 +3,7 @@ package co.edu.eafit.coglovi.restservice.preguntas;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,11 +12,16 @@ import co.edu.eafit.coglovi.exception.CoGloViException;
 import co.edu.eafit.coglovi.manager.pregunta.PreguntaManager;
 import co.edu.eafit.coglovi.model.Constantes;
 import co.edu.eafit.coglovi.model.core.RestResponse;
+import co.edu.eafit.coglovi.model.ddd.Estado;
 import co.edu.eafit.coglovi.model.ddd.Pregunta;
+import co.edu.eafit.coglovi.model.usuario.GrupoInteres;
+import co.edu.eafit.coglovi.model.usuario.UsuarioAPP;
 import co.edu.eafit.coglovi.transversal.PropertiesManager;
 
 import com.google.gson.Gson;
 
+@Controller
+@RequestMapping("preguntas")
 public class PreguntasRest {
 	private final Log logger = LogFactory.getLog(getClass());
 	
@@ -44,5 +50,17 @@ public class PreguntasRest {
 			logger.error(e.getMessage(), e);
 		}	
 		return restResponse;
+	}
+	public static void main(String[] args) {
+		Gson gson=new Gson();
+		Pregunta pregunta=new Pregunta();
+		GrupoInteres grupoInteres=new GrupoInteres();
+		UsuarioAPP usuario=new UsuarioAPP();
+		usuario.setCorreoElectronico("juan3116162188@gmail.com");
+		grupoInteres.setGruposInteres("CI");
+		pregunta.setUsuario(usuario);
+		pregunta.setPregunta("Cuanto tiempo vive una tortuga");
+		pregunta.setGrupoInteres(grupoInteres);
+		System.out.println(gson.toJson(pregunta));
 	}
 }
